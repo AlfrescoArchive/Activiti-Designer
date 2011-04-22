157df4dd2e5c918215735cf700028d315cd9d1be
tiesebarrell <tiesebarrell@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-04-22 18:30:42 +0000

    RESOLVED - issue ACT-477: Creating new diagram using wizard with existing name overwrites without warning http://jira.codehaus.org/browse/ACT-477
    * Added default filename and selection switch to src/main/resources/diagrams if the project is the selection;

--- org.activiti.designer.eclipse/src/main/java/org/activiti/designer/eclipse/ui/wizard/diagram/CreateDefaultActivitiDiagramNameWizardPage.java
+++ org.activiti.designer.eclipse/src/main/java/org/activiti/designer/eclipse/ui/wizard/diagram/CreateDefaultActivitiDiagramNameWizardPage.java
@@ -4,8 +4,11 @@ import org.activiti.designer.eclipse.common.ActivitiBPMNDiagramConstants;
 import org.activiti.designer.eclipse.common.ActivitiPlugin;
 import org.activiti.designer.eclipse.common.PluginImage;
 import org.apache.commons.lang.StringUtils;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
 
@@ -13,14 +16,36 @@ public class CreateDefaultActivitiDiagramNameWizardPage extends WizardNewFileCre
 
   public static final String PAGE_NAME = "createDefaultActivitiDiagramNameWizardPage";
 
+  private static final String DEFAULT_DIAGRAM_NAME = "My Process";
+
   public CreateDefaultActivitiDiagramNameWizardPage(IStructuredSelection selection) {
     super(PAGE_NAME, selection);
+    super.setFileName(DEFAULT_DIAGRAM_NAME);
     setTitle("New Activiti Diagram");
     setImageDescriptor(ActivitiPlugin.getImageDescriptor(PluginImage.ACTIVITI_LOGO_64x64));
     setDescription("Create a new Activiti BPMN 2.0 Diagram.");
     setFileExtension(StringUtils.substringAfter(ActivitiBPMNDiagramConstants.DIAGRAM_EXTENSION, "."));
-  }
 
+    // Set selection to the default folder if the project itself was selected
+    final IProject project = getProjectFromSelection(selection);
+    if (project != null) {
+      final IFolder folder = project.getFolder(ActivitiBPMNDiagramConstants.DIAGRAM_FOLDER);
+      if (folder != null && folder.exists()) {
+        selection.toList().set(0, folder);
+      }
+    }
+  }
+  private IProject getProjectFromSelection(final IStructuredSelection selection) {
+    IProject result = null;
+    if (selection.size() == 1) {
+      final Object firstSelectionElement = selection.getFirstElement();
+      if (firstSelectionElement instanceof IJavaProject) {
+        final IJavaProject javaProject = (IJavaProject) firstSelectionElement;
+        result = javaProject.getProject();
+      }
+    }
+    return result;
+  }
   @Override
   public boolean isPageComplete() {
     return getWizard().canFinish();
