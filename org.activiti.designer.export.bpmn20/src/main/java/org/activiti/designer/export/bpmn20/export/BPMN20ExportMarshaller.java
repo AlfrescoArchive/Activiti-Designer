e9fdef522d587ac7114c4cf96ab3a3d331775476
tiesebarrell <tiesebarrell@51b7ce9a-3489-0410-86de-dc9a27cf12c7>
2011-08-07 09:31:35 +0000

    IN PROGRESS - issue ACT-894: Make the validator ID overridable and export BPMN20ExportMarshaller http://jira.codehaus.org/browse/ACT-894

--- org.activiti.designer.export.bpmn20/src/main/java/org/activiti/designer/export/bpmn20/export/BPMN20ExportMarshaller.java
+++ org.activiti.designer.export.bpmn20/src/main/java/org/activiti/designer/export/bpmn20/export/BPMN20ExportMarshaller.java
@@ -14,7 +14,9 @@ import javax.xml.stream.XMLStreamWriter;
 import org.activiti.designer.eclipse.common.ActivitiBPMNDiagramConstants;
 import org.activiti.designer.eclipse.extension.export.AbstractExportMarshaller;
 import org.activiti.designer.eclipse.extension.export.ExportMarshaller;
+import org.activiti.designer.eclipse.extension.validation.ProcessValidator;
 import org.activiti.designer.eclipse.preferences.PreferencesUtil;
+import org.activiti.designer.eclipse.util.ExtensionPointUtil;
 import org.activiti.designer.util.preferences.Preferences;
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.bpmn2.AlfrescoScriptTask;
@@ -85,17 +87,18 @@ public class BPMN20ExportMarshaller extends AbstractExportMarshaller implements 
     this.monitor = monitor;
     this.diagram = diagram;
 
-    monitor.beginTask("Exporting to BPMN 2.0", WORK_TOTAL);
+    this.monitor.beginTask("Exporting to BPMN 2.0", WORK_TOTAL);
 
     // Clear problems for this diagram first
     clearMarkers(getResource(diagram.eResource().getURI()));
 
     boolean performMarshalling = true;
 
-    // Validate if the BPMN validator is checked in the preferences
-    if (PreferencesUtil.getBooleanPreference(Preferences.VALIDATE_ACTIVITI_BPMN_FORMAT)) {
+    // Retrieve validatorId to allow for overriding the default validator
+    String validatorId = getValidatorId();
 
-      boolean validBpmn = invokeValidator(ActivitiBPMNDiagramConstants.BPMN_VALIDATOR_ID, diagram, new SubProgressMonitor(monitor, WORK_VALIDATION));
+    if (isValidationEnabled(validatorId)) {
+      boolean validBpmn = invokeValidator(validatorId, diagram, new SubProgressMonitor(this.monitor, WORK_VALIDATION));
 
       if (!validBpmn) {
 
@@ -111,14 +114,42 @@ public class BPMN20ExportMarshaller extends AbstractExportMarshaller implements 
         }
       }
     } else {
-      monitor.worked(WORK_VALIDATION);
+      this.monitor.worked(WORK_VALIDATION);
     }
 
     if (performMarshalling) {
       marshallBPMNDiagram();
-      monitor.worked(WORK_EXPORT);
+      this.monitor.worked(WORK_EXPORT);
     }
-    monitor.done();
+    this.monitor.done();
+  }
+  /**
+   * Gets the identifier of the {@link ProcessValidator} to be invoked from this
+   * marshaller. This implementation provides the default value of
+   * {@link ActivitiBPMNDiagramConstants#BPMN_VALIDATOR_ID}. Override this
+   * method to use this export marshaller for all BPMN 2.0 marshalling, but use
+   * your own {@link ProcessValidator} instead of the one provided by Activiti
+   * Designer by default.
+   * 
+   * @return The string identifier of the {@link ProcessValidator}, may not be
+   *         null or empty or validation will be skipped.
+   */
+  public String getValidatorId() {
+    return ActivitiBPMNDiagramConstants.BPMN_VALIDATOR_ID;
+  }
+
+  private boolean isValidationEnabled(String validatorId) {
+
+    boolean result = true;
+
+    if (ActivitiBPMNDiagramConstants.BPMN_VALIDATOR_ID.equals(validatorId)) {
+      // Validate if the BPMN validator is checked in the preferences
+      result = PreferencesUtil.getBooleanPreference(Preferences.VALIDATE_ACTIVITI_BPMN_FORMAT);
+    } else {
+      result = StringUtils.isNotBlank(validatorId) && ExtensionPointUtil.getProcessValidator(validatorId) != null;
+    }
+
+    return result;
   }
 
   private void marshallBPMNDiagram() {
@@ -159,10 +190,10 @@ public class BPMN20ExportMarshaller extends AbstractExportMarshaller implements 
       xtw.writeNamespace(OMGDI_PREFIX, OMGDI_NAMESPACE);
       xtw.writeAttribute("typeLanguage", SCHEMA_NAMESPACE);
       xtw.writeAttribute("expressionLanguage", XPATH_NAMESPACE);
-      if(process != null && StringUtils.isNotEmpty(process.getNamespace())) {
-      	xtw.writeAttribute("targetNamespace", process.getNamespace());
+      if (process != null && StringUtils.isNotEmpty(process.getNamespace())) {
+        xtw.writeAttribute("targetNamespace", process.getNamespace());
       } else {
-      	xtw.writeAttribute("targetNamespace", PROCESS_NAMESPACE);
+        xtw.writeAttribute("targetNamespace", PROCESS_NAMESPACE);
       }
 
       // start process element
@@ -179,12 +210,12 @@ public class BPMN20ExportMarshaller extends AbstractExportMarshaller implements 
       }
 
       for (EObject object : contents) {
-      	if(object instanceof FlowNode) {
-      		FlowNode node = (FlowNode) object;
-      		if(node.getIncoming().size() == 0 && node.getOutgoing().size() == 0) {
-      			continue;
-      		}
-      	}
+        if (object instanceof FlowNode) {
+          FlowNode node = (FlowNode) object;
+          if (node.getIncoming().size() == 0 && node.getOutgoing().size() == 0) {
+            continue;
+          }
+        }
         createXML(object, "");
       }
 
@@ -218,61 +249,57 @@ public class BPMN20ExportMarshaller extends AbstractExportMarshaller implements 
       xtw.writeAttribute("id", subProcessId + startEvent.getId());
       xtw.writeAttribute("name", startEvent.getName());
 
-      if(startEvent.getFormKey() != null && startEvent.getFormKey().length() > 0) {
+      if (startEvent.getFormKey() != null && startEvent.getFormKey().length() > 0) {
         xtw.writeAttribute(ACTIVITI_EXTENSIONS_PREFIX, ACTIVITI_EXTENSIONS_NAMESPACE, "formKey", startEvent.getFormKey());
       }
-      
-      if(startEvent.getInitiator() != null && startEvent.getInitiator().length() > 0) {
+
+      if (startEvent.getInitiator() != null && startEvent.getInitiator().length() > 0) {
         xtw.writeAttribute(ACTIVITI_EXTENSIONS_PREFIX, ACTIVITI_EXTENSIONS_NAMESPACE, "initiator", startEvent.getInitiator());
       }
-      
-      if(startEvent.getEventDefinitions().size() > 0) {
-      	
-      	TimerEventDefinition timerDef = (TimerEventDefinition) startEvent.getEventDefinitions().get(0);
-      	
-      	if(timerDef.getTimeDuration() != null && 
-            ((((FormalExpression) timerDef.getTimeDuration()).getBody() != null && 
-                    ((FormalExpression) timerDef.getTimeDuration()).getBody().length() > 0) ||
-                    
-                    (((FormalExpression) timerDef.getTimeDate()).getBody() != null && 
-                            ((FormalExpression) timerDef.getTimeDate()).getBody().length() > 0) ||
-                            
-                            (((FormalExpression) timerDef.getTimeCycle()).getBody() != null && 
-                                    ((FormalExpression) timerDef.getTimeCycle()).getBody().length() > 0))) {
-      	
-	      	xtw.writeStartElement("timerEventDefinition");
-	        
-	        if(((FormalExpression) timerDef.getTimeDuration()).getBody() != null && 
-	                      ((FormalExpression) timerDef.getTimeDuration()).getBody().length() > 0) {
-	          
-	          xtw.writeStartElement("timeDuration");
-	          xtw.writeCharacters(((FormalExpression) timerDef.getTimeDuration()).getBody());
-	          xtw.writeEndElement();
-	        
-	        } else if(((FormalExpression) timerDef.getTimeDate()).getBody() != null && 
-	                      ((FormalExpression) timerDef.getTimeDate()).getBody().length() > 0) {
-	          
-	          xtw.writeStartElement("timeDate");
-	          xtw.writeCharacters(((FormalExpression) timerDef.getTimeDate()).getBody());
-	          xtw.writeEndElement();
-	        
-	        } else {
-	          
-	          xtw.writeStartElement("timeCycle");
-	          xtw.writeCharacters(((FormalExpression) timerDef.getTimeCycle()).getBody());
-	          xtw.writeEndElement();
-	        }
-	        
-	        xtw.writeEndElement();
-      	}
+
+      if (startEvent.getEventDefinitions().size() > 0) {
+
+        TimerEventDefinition timerDef = (TimerEventDefinition) startEvent.getEventDefinitions().get(0);
+
+        if (timerDef.getTimeDuration() != null
+                && ((((FormalExpression) timerDef.getTimeDuration()).getBody() != null && ((FormalExpression) timerDef.getTimeDuration()).getBody().length() > 0)
+                        ||
+
+                        (((FormalExpression) timerDef.getTimeDate()).getBody() != null && ((FormalExpression) timerDef.getTimeDate()).getBody().length() > 0) ||
+
+                (((FormalExpression) timerDef.getTimeCycle()).getBody() != null && ((FormalExpression) timerDef.getTimeCycle()).getBody().length() > 0))) {
+
+          xtw.writeStartElement("timerEventDefinition");
+
+          if (((FormalExpression) timerDef.getTimeDuration()).getBody() != null && ((FormalExpression) timerDef.getTimeDuration()).getBody().length() > 0) {
+
+            xtw.writeStartElement("timeDuration");
+            xtw.writeCharacters(((FormalExpression) timerDef.getTimeDuration()).getBody());
+            xtw.writeEndElement();
+
+          } else if (((FormalExpression) timerDef.getTimeDate()).getBody() != null && ((FormalExpression) timerDef.getTimeDate()).getBody().length() > 0) {
+
+            xtw.writeStartElement("timeDate");
+            xtw.writeCharacters(((FormalExpression) timerDef.getTimeDate()).getBody());
+            xtw.writeEndElement();
+
+          } else {
+
+            xtw.writeStartElement("timeCycle");
+            xtw.writeCharacters(((FormalExpression) timerDef.getTimeCycle()).getBody());
+            xtw.writeEndElement();
+          }
+
+          xtw.writeEndElement();
+        }
       }
-      
-      if(startEvent.getFormProperties().size() > 0) {
+
+      if (startEvent.getFormProperties().size() > 0) {
         xtw.writeStartElement("extensionElements");
         FormPropertiesExport.createFormPropertiesXML(startEvent.getFormProperties(), xtw);
         xtw.writeEndElement();
       }
-      
+
       // end StartEvent element
       xtw.writeEndElement();
 
@@ -282,10 +309,10 @@ public class BPMN20ExportMarshaller extends AbstractExportMarshaller implements 
       xtw.writeStartElement("endEvent");
       xtw.writeAttribute("id", subProcessId + endEvent.getId());
       xtw.writeAttribute("name", endEvent.getName());
-      
-      if(endEvent.getEventDefinitions().size() > 0) {
+
+      if (endEvent.getEventDefinitions().size() > 0) {
         ErrorEventDefinition errorDef = (ErrorEventDefinition) endEvent.getEventDefinitions().get(0);
-        if(errorDef.getErrorCode() != null && errorDef.getErrorCode().length() > 0) {
+        if (errorDef.getErrorCode() != null && errorDef.getErrorCode().length() > 0) {
           xtw.writeStartElement("errorEventDefinition");
           xtw.writeAttribute("errorRef", errorDef.getErrorCode());
           xtw.writeEndElement();
@@ -315,13 +342,13 @@ public class BPMN20ExportMarshaller extends AbstractExportMarshaller implements 
 
     } else if (object instanceof ReceiveTask) {
       ReceiveTaskExport.createReceiveTask(object, subProcessId, xtw);
-    
+
     } else if (object instanceof BusinessRuleTask) {
       BusinessRuleTaskExport.createBusinessRuleTask(object, subProcessId, xtw);
-    
+
     } else if (object instanceof AlfrescoScriptTask) {
       AlfrescoScriptTaskExport.createScriptTask(object, subProcessId, xtw);
-      
+
     } else if (object instanceof CallActivity) {
       CallActivityExport.createCallActivity(object, subProcessId, xtw);
 
@@ -349,24 +376,24 @@ public class BPMN20ExportMarshaller extends AbstractExportMarshaller implements 
 
       // end ExclusiveGateway element
       xtw.writeEndElement();
-      
+
     } else if (object instanceof BoundaryEvent) {
       BoundaryEventExport.createBoundaryEvent(object, subProcessId, xtw);
 
     } else if (object instanceof SubProcess) {
       SubProcess subProcess = (SubProcess) object;
       List<FlowElement> flowElementList = subProcess.getFlowElements();
-      if(flowElementList != null && flowElementList.size() > 0) {
+      if (flowElementList != null && flowElementList.size() > 0) {
         // start SubProcess element
         xtw.writeStartElement("subProcess");
         xtw.writeAttribute("id", subProcess.getId());
         if (subProcess.getName() != null) {
           xtw.writeAttribute("name", subProcess.getName());
         }
-        
+
         ExtensionListenerExport.createExtensionListenerXML(subProcess.getActivitiListeners(), true, EXECUTION_LISTENER, xtw);
         MultiInstanceExport.createMultiInstance(object, subProcessId, xtw);
-        
+
         for (FlowElement flowElement : flowElementList) {
           createXML(flowElement, subProcess.getId());
         }
@@ -377,22 +404,4 @@ public class BPMN20ExportMarshaller extends AbstractExportMarshaller implements 
     }
   }
 
-  /*private void createSubProcessXML(XMLStreamWriter xtw, String subProcessId) throws Exception {
-
-    IResource resource = getResource(Util.getSubProcessURI(diagram, subProcessId));
-
-    ResourceSet set = new ResourceSetImpl();
-    final Diagram diagram = GraphitiUiInternal.getEmfService().getDiagramFromFile((IFile) resource, set);
-
-    EList<EObject> contents = diagram.eResource().getContents();
-
-    for (EObject object : contents) {
-      createXML(object, xtw, subProcessId + "_");
-    }
-  }*/
-
-  @Deprecated
-  private void createErrorMessage(String message) {
-    addProblemToDiagram(diagram, message, null);
-  }
 }
