3421dcfa090fcb47df3a207ea8707eb12d0e8033
Tijs Rademakers <tijs.rademakers@gmail.com>
2016-08-18 15:40:44 +0200

    Added license headers

--- org.activiti.designer.kickstart.eclipse/src/main/java/org/activiti/designer/kickstart/eclipse/preferences/PreferencesUtil.java
+++ org.activiti.designer.kickstart.eclipse/src/main/java/org/activiti/designer/kickstart/eclipse/preferences/PreferencesUtil.java
@@ -1,4 +1,17 @@
 /**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
  * 
  */
 package org.activiti.designer.kickstart.eclipse.preferences;
@@ -80,7 +93,7 @@ public final class PreferencesUtil {
     List<String> resultList = new ArrayList<String>();
     String defaultFormTypes = PreferencesUtil.getStringPreference(preferenceName);
     if(defaultFormTypes != null && defaultFormTypes.length() > 0) {
-      String[] formList = defaultFormTypes.split("±");
+      String[] formList = defaultFormTypes.split("ï¿½");
       if (formList != null) {
         for (String formKey : formList) {
           resultList.add(formKey);
